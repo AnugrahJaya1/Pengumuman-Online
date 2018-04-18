f5da00f389cdf7c7822b761d935a715d8358d2d5
Gabriel Panji Lazuardi <32382701+Bephometh@users.noreply.github.com>
2018-04-18 22:39:11 +0700

    Add files via upload

---
+++ Online Anouncment/Layout/bulletin.php
@@ -0,0 +1,39 @@
+
+    <style>
+    .bulletin {
+        list-style: none;
+        padding: 5% 5%;
+        background-color: #d7cbcb;
+        border-bottom: solid 4px white;
+    }
+
+    #bullCon {
+        flex : 1;
+        opacity: 0.8;
+        display: block;
+        width: 60%;
+    }
+
+</style>
+
+<div id="bullCon">
+    <ul id="annoucmentBull" style="text-decoration:none;">
+        <li class="bulletin">
+            <article>
+                2. Annoucment Goes Here
+            </article>
+        </li>
+        <li class="bulletin">
+            <article>
+                2. Annoucment Goes Here
+            </article>
+        </li>
+        <li class="bulletin">
+            <article>
+                3. Annoucment Goes Here
+            </article>
+        </li>
+    </ul>
+</div>
+
+
