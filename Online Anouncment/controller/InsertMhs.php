2bf69e5e08f82ac23b4db39a9011e285bfc52a74
Anugrah Jaya <30459937+AnugrahJaya1@users.noreply.github.com>
2018-04-21 08:50:52 +0700

    benerin error

--- Online Anouncment/controller/InsertMhs.php
+++ Online Anouncment/controller/InsertMhs.php
@@ -1,11 +1,19 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "pengumuman online");
+    include("../connection/connectionAdm.php");
 
-$npm = $_GET['npm'];
-$email = $_GET['emailMhs'];
-$pass = $_GET['passMhs'];
+    if(isset($_POST['add'])){
+        if(empty($_POST['add']) || empty($_POST['emailMhs']) || empty($_POST['passMhs'])){
+            header("Location: ../adminInsert.php");
+        }else{
+            $npm = $_POST['npm'];
+            $email = $_POST['emailMhs'];
+            $pass = md5($_POST['passMhs']);
 
-$sql = "INSERT INTO mahasiswa (NPM, EmailMahasiswa, PasswordMahasiswa) VALUES ('$npm','$email','$pass')";
-$mysqli -> query($sql);
-header('Location : ../adminInsert.php');
+            $sql = "INSERT INTO mahasiswa (NPM, EmailMahasiswa, PasswordMahasiswa) VALUES ('$npm','$email','$pass')";
+
+            $mysqli -> query($sql);
+        }
+    }
+    
+    header("Location: ../adminInsert.php");
 ?>
