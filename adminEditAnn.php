5cd566a5b7c5982ed3ae07e4d89d5e0ed6358582
Gabriel Panji Lazuardi <32382701+Bephometh@users.noreply.github.com>
2018-04-22 20:22:26 +0700

    added Insert Tag, Edit Mahasiswa, Edit Pengumuman.
    Dan ada update di layout admin Profile

---
+++ adminEditAnn.php
@@ -0,0 +1,85 @@
+
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <style>
+        /*CSS for the body*/
+        #hugeCon {
+            display: flex;
+        }
+        #annSearch{
+            margin-left : 4px;
+            margin-top : 20px;
+            border-bottom : solid 2px black;
+            height : 8%;
+        }
+        #search{
+            width : 40%;
+        }
+        #searchButt{
+            margin-top : 4px;
+        }
+
+        #announceText {
+            padding-top: 30px;
+            width: 80%;
+            height: 80%;
+        }
+
+        #txtField {
+            width: 90%;
+            height: 600px;
+            padding-top: 10px;
+        }
+
+        #titForm {
+            width: 90%;
+            height: 20%;
+        }
+
+        #titDate {
+            margin-bottom: 5px;
+            width: 18.2%;
+            height: 20%;
+        }
+
+        #titAuth {
+            margin-top: 4px;
+            margin-bottom: 5px;
+        }
+    </style>
+    <meta charset="utf-8" />
+    <title>Edit Annoucment</title>
+</head>
+<body>
+    <!--Top NavBar-->
+    <?php include("Layout/header.php"); ?>
+    <div id="hugeCon">
+        <div id="formCon" >
+            <!-- Annoucment Search section-->
+            <form id="annSearch" method="get" action="">
+                Search Article <br />
+                <input type="text" id="search" name="searchAnn" placeholder="Announcement Title" />
+                <br />
+                <input type="button" id="searchButt" name="searchButt" value="Search" />
+            </form>
+            <form id="announceText" method="get" action="controller/InsertAnnouncment.php">
+                <fieldset>
+                    <legend>Write Announcment here </legend>
+                    <br />
+                    <input id="titForm"  type="text" name="annTitle" />
+                    <input id="titAuth"  type="text" name="annAuth" />
+                    <input id="titDate" type="date" name="annDate" value="" />
+                    <textarea name="annText" id="txtField"></textarea>
+                    <br />
+                    <input type="submit" name="annButt" id="annButt" value="Edit" />
+                </fieldset>
+            </form>
+        </div>
+        
+        <!--Profile Section-->
+        <?php include("Layout/adminProf.php");?>
+    </div>
+</body>
+</html>
---
+++ adminInsert.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<style>
+    #hugeCon {
+        padding-top : 20px;
+        display: flex;
+    }
+    #admnIns {
+        padding-top : 20px;
+        background-color: lightgrey;
+        flex: 1;
+        opacity: 0.8;
+        display: block;
+        width: 60%;
+    }
+    #fomrIns{
+        margin-left : 10%;
+    }
+    .forms{
+        width : 90%;
+        margin-left : 2%;
+    }
+    
+</style>
+<head>
+    <meta charset="utf-8" />
+    <title>Insert Mahasiswa</title>
+</head>
+<body>
+    <!--NavBar-->
+    <?php include("Layout/header.php");?>
+    <div id="hugeCon">
+        <!--Edit Section-->
+        <div id="admnIns">
+            <form id="fomrIns" method="post" action="controller/InsertMhs.php"  style="padding-left : 10px;">
+                NPM :<br />
+                <input class="forms" name="npm" type="text"/> <br /> <br />
+                Email :<br />
+                <input class="forms" name="emailMhs" type="email" /><br /> <br />
+                Password :
+                <input class="forms" name="passMhs" type="password" />
+                <br />
+                <br />
+                <input type="submit" value="Add" name="add"/>
+            </form>
+        </div>
+        <!--Profile Section-->
+        <?php include("Layout/adminProf.php");?>
+    </div>
+</body>
+</html>
