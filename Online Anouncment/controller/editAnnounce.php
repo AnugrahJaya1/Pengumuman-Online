ca5c67e6e8e4e8ec22bb13f0490c029b26d56c5c
Anugrah Jaya <30459937+AnugrahJaya1@users.noreply.github.com>
2018-04-24 11:41:23 +0700

    sudah bisa edit Announce

--- Online Anouncment/controller/editAnnounce.php
+++ Online Anouncment/controller/editAnnounce.php
@@ -1,23 +1,40 @@
 <?php
-   include("connection/connectionAdm.php");
+   include("../connection/connectionAdm.php");
 
    
 
-   if(isset$_GET['annButt']){
+   if(isset($_GET['iUp'])){
        $judul=$_GET['annTitle'];
        $author=$_GET['annAuth'];
        $tanggal=$_GET['annDate'];
        $des=$_GET['annText'];
 
        if(isset($judul) && isset($author) && isset($tanggal) && isset($des) 
-                    && $judul!="" && $author!="" && $tanggal!="" && $des!=""){
-           $sql="UPDATE pengumuman SET Judul='$judul',Author='$author',Tanggal='$tanggal',Deskripsi='$des'";
+                && $judul!="" && $author!="" && $tanggal!="" && $des!=""){
+           $sql="UPDATE pengumuman SET Judul='$judul',Author='$author',Tanggal='$tanggal',Deskripsi='$des' WHERE Judul='$judul'";
            $mysqli->query($sql);
-           $sql="UPDATE tagpengumuman SET Judul='$judul',Author='$author',Tanggal='$tanggal',Deskripsi='$des'"
+           $sql="UPDATE tagpengumuman SET Judul='$judul',Author='$author',Tanggal='$tanggal',Deskripsi='$des' WHERE Judul=$judul";
            $mysqli->query($sql);
-
-           //belum beres
        }
+   }else if(isset($_GET['iDel'])){
+        $judul=$_GET['annTitle'];
+        $author=$_GET['annAuth'];
+        $tanggal=$_GET['annDate'];
+        $des=$_GET['annText'];
+
+        if(isset($judul) && isset($author) && isset($tanggal) && isset($des) 
+             && $judul!="" && $author!="" && $tanggal!="" && $des!=""){
+
+
+            $sql="DELETE FROM pengumuman WHERE Judul='$judul";
+            $mysqli->query($sql);
+
+            $sql="DELETE FROM tagpengumuman WHERE Judul='$Judul'";
+            $mysqli->query($sql);
+        }
+
    }
+
+   header("Location: ../adminEditAnn.php");
     
 ?>
