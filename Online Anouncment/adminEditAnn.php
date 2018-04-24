c4398078a81c077264f8491ef60ab35c975c2bb3
Anugrah Jaya <30459937+AnugrahJaya1@users.noreply.github.com>
2018-04-24 11:40:49 +0700

    comment form

--- Online Anouncment/adminEditAnn.php
+++ Online Anouncment/adminEditAnn.php
@@ -69,15 +69,8 @@
     <?php include("Layout/header.php"); ?>
     <div id="hugeCon">
         <div id="formCon" >
-            <!-- Annoucment Search section-->
-            <form id="annSearch" method="get" action="">
-                Search Article <br />
-                <input type="text" id="search" name="searchAnn" placeholder="Announcement Title" />
-                <br />
-                <input type="button" id="searchButt" name="searchButt" value="Search" />
-            </form>
-            <br>
-            <table>
+
+         <table>
                 <tr>
                     <th>Judul</th>
                     <th>Author</th>
@@ -88,19 +81,29 @@
                 ?>
 
             </table>
+            <br>
+            <!-- Annoucment Search section-->
+            <form id="annSearch" method="get" action="controller/searchAnnAdm.php">
+                Search Article <br />
+                <input type="text" id="search" name="searchAnn" placeholder="Announcement Title" />
+                <br />
+                <input type="submit" id="searchButt" name="searchButt" value="Search" />
+            </form>
+            <br>
+           
 
-            <form id="announceText" method="get" action="controller/editAnnounce.php">
+            <!-- <form id="announceText" method="get" action="controller/editAnnounce.php">
                 <fieldset>
                     <legend>Write Announcment here </legend>
                     <br />
-                    <input id="titForm"  type="text" name="annTitle" />
+                    <input id="titForm"  type="text" name="annTitle" >" />
                     <input id="titAuth"  type="text" name="annAuth" />
                     <input id="titDate" type="date" name="annDate" value="" />
                     <textarea name="annText" id="txtField"></textarea>
                     <br />
                     <input type="submit" name="annButt" id="annButt" value="Edit" />
                 </fieldset>
-            </form>
+            </form> -->
         </div>
         
         <!--Profile Section-->
