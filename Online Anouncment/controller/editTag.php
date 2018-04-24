618e46e1eded94f6bf8d91cdd5c5ba4b7970ee3d
Anugrah Jaya <30459937+AnugrahJaya1@users.noreply.github.com>
2018-04-24 12:32:13 +0700

    bisa update dan delete

--- Online Anouncment/controller/editTag.php
+++ Online Anouncment/controller/editTag.php
@@ -3,7 +3,7 @@
 
    
 
-    if(isset($_GET['subButt'])){
+    if(isset($_GET['iUp'])){
         $newName=$_GET['newName'];
         $oldName=$_GET['oldName'];
         if(isset($newName) && $newName!="" && isset($oldName) && $oldName!="" ){
@@ -16,7 +16,21 @@
             $sql="UPDATE pengumumanmahasiswa SET NamaTag='$newName' WHERE NamaTag='$oldName'";
             $mysqli->query($sql);
 
-            header("Location: ../adminEditTag.php");
+            
+        }
+    }else if(isset($_GET['iDel'])){
+        $name=$_GET['oldName'];
+        if(isset($name) && $name!=""){
+            $sql="DELETE FROM tag WHERE NamaTag='$name'";
+            $mysqli->query($sql);
+
+            $sql="DELETE FROM tagpengumuman WHERE NamaTag='$name'";
+            $mysqli->query($sql);
+
+            $sql="DELETE FROM pengumumanmahasiswa WHERE NamaTag='$name'";
+            $mysqli->query($sql);
         }
     }
+
+    header("Location: ../adminEditTag.php");
 ?>
