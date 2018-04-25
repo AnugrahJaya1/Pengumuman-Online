98c8eaf0ffe38b877e58127ac79005b9a3bb7a5a
Anugrah Jaya <30459937+AnugrahJaya1@users.noreply.github.com>
2018-04-25 19:09:29 +0700

    Add files via upload

---
+++ Online Anouncment/controller/getTag.php
@@ -0,0 +1,14 @@
+<?php
+    include("connection/connectionAdm.php");
+
+    $sql="SELECT DISTINCT NamaTag FROM tag";
+
+    if($result=$mysqli->query($sql)){
+        while($row=$result->fetch_array()){
+            $name=$row['NamaTag'];
+            echo "<label for='$name'>".$name."</label>";
+            echo "<input type='checkbox' name='$name' value='$name' />";
+        }
+    }
+
+?>
