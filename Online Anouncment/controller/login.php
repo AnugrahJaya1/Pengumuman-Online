cb0ab94141d996e0450f10181aebbfa73bf9cb49
Gabriel Panji Lazuardi <32382701+Bephometh@users.noreply.github.com>
2018-04-25 17:28:45 +0700

    Add files via upload

--- Online Anouncment/controller/login.php
+++ Online Anouncment/controller/login.php
@@ -8,10 +8,10 @@
 
 		if(empty($_POST['email'])) {
 			array_push($error, "Email Doesn't Exist");
-			header('Location: ../LoginPage.php');
+			header('Location: ../index.php');
 		} else if( empty($_POST['password'])){
 			array_push($error, "Wrong password ");
-			header('Location: ../LoginPage.php');
+			header('Location: ../index.php');
 		}else{
 			$email=$_POST['email'];
 			$password=md5($_POST['password']);
@@ -42,13 +42,13 @@
 				if($row['PasswordMahasiswa']==$password ){
 					header('Location: ../UserPage.php');//redirect ke UserPage
 				}else if($row['PasswordAdmin']==$password){
-					header('Location: ../index.php');//redirect ke index(admin page)
+					header('Location: ../adminPAge.php');//redirect ke index(admin page)
 				}else{
-					header('Location: ../LoginPage.php');
+					header('Location: ../index.php');
 				}
 			}else{
 				//echo"Error : Email $email does exist";
-				header('Location: ../LoginPage.php');
+				header('Location: ../index.php');
 				
 			}
 		}
