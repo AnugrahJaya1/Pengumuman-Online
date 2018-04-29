56aa454437cfee4bbbc02204f4144cdbe28010c8
Anugrah Jaya <30459937+AnugrahJaya1@users.noreply.github.com>
2018-04-29 12:03:49 +0700

    Add files via upload

--- Online Anouncment/controller/InsertAnnouncment.php
+++ Online Anouncment/controller/InsertAnnouncment.php
@@ -1,14 +1,17 @@
 <?php
 	include("../connection/connectionAdm.php");
 
-	$title = $_GET['annTitle'];
-	$auth = $_GET['annAuth'];
-	$date = $_GET['annDate'];
-	$announce = $_GET['annText'];
+	if(isset($_GET['annButt'])){
+		$title = $_GET['annTitle'];
+		$auth = $_GET['annAuth'];
+		$date = $_GET['annDate'];
+		$announce = $_GET['annText'];
 
-	$sql = "INSERT INTO pengumuman (Judul, Author, Deskripsi, Tanggal) VALUES ('$title', '$auth', '$announce', '$date') ";
-	
-	$mysqli->query($sql);
-	
-	include("../index.php");
+		if($title!="" && $auth!="" && $date!="" && $announce!=""){
+			$sql = "INSERT INTO pengumuman (Judul, Author, Deskripsi, Tanggal) VALUES ('$title', '$auth', '$announce', '$date') ";
+		
+			$mysqli->query($sql);
+		}
+	}
+	header("Location: ../adminAnnounce.php");
 ?>
